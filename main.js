2b563d6b23c8682a1e07c905521a991fa129ce36
Andrew Allison <andrewallison5211@gmail.com>
2024-11-18 20:48:16 -0800

    blobby sphere

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import { ThreeMFLoader } from "three/examples/jsm/Addons.js";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Sets the color of the background.
-renderer.setClearColor(0xFEFEFE);
+// renderer.setClearColor(0xFEFEFE);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -24,21 +25,29 @@ camera.position.set(6, 8, 14);
 // Has to be done everytime we update the camera position.
 orbit.update();
 
-// Creates a 12 by 12 grid helper.
-const gridHelper = new THREE.GridHelper(12, 12);
-scene.add(gridHelper);
+const uniforms = {
+    u_time: { value: 0.0 }
+};
 
-// Creates an axes helper with an axis length of 4.
-const axesHelper = new THREE.AxesHelper(4);
-scene.add(axesHelper);
+const mat = new THREE.ShaderMaterial({
+    wireframe: true,
+    vertexShader: document.getElementById('vertexshader')?.textContent,
+    fragmentShader: document.getElementById('fragmentshader')?.textContent
+});
 
+const geo = new THREE.IcosahedronGeometry(4, 30);
+const mesh = new THREE.Mesh(geo, mat);
+scene.add(mesh);
+
+const clock = new THREE.Clock();
 function animate() {
+    uniforms.u_time.value = clock.getElapsedTime();
     renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate);
 
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
